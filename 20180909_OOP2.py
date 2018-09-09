1e4deb173ea6940088d6239d49070d65710d01b4
SnakeT <mygooogle@163.com>
2018-09-10 00:10:04 +0800

    subclass

---
+++ 20180909_OOP2.py
@@ -0,0 +1,12 @@
+# /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+	def run(self):
+		print('Animal is running...')
+		
+class Dog(Animal):
+	pass
+	
+class Cat(Animal):
+	pass
