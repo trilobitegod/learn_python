d612d3a940000d468bf2621e3d770d1be8d31267
SnakeT <mygooogle@163.com>
2018-09-10 00:32:34 +0800

    n

--- 20180909_OOP2.py
+++ 20180909_OOP2.py
@@ -6,7 +6,33 @@ class Animal(object):
 		print('Animal is running...')
 		
 class Dog(Animal):
-	pass
+	
+	def run(self):
+		print('Dog is running...')
+		
+	def eat(self):
+		print('Eating meat...')
 	
 class Cat(Animal):
 	pass
+	
+def run_twice(animal):
+	animal.run()
+	animal.run()
+	
+dog=Dog()
+dog.run()
+run_twice(Animal())
+run_twice(Dog())
+
+print(isinstance(dog,Animal))
+print(isinstance(dog,Dog))
+print(isinstance(dog,Cat))
+
+
+class Student(object):
+	def __init__(self,name):
+		self.name=name
+s=Student('Bob')
+s.score=90
+s.city=BeiJing
