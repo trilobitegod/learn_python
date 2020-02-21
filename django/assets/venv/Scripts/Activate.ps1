446a320f6e335f3926aebd70603f2655ef80964e
SnakeT <mygooogle@163.com>
2020-02-22 01:15:43 +0800

    change asserts to assets

--- django/assets/venv/Scripts/Activate.ps1
+++ django/assets/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="F:\Documents\Git\learn_python\django\asserts\venv"
+$env:VIRTUAL_ENV="F:\Documents\Git\learn_python\django\assets\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- django/assets/venv/Scripts/activate.bat
+++ django/assets/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=F:\Documents\Git\learn_python\django\asserts\venv"
+set "VIRTUAL_ENV=F:\Documents\Git\learn_python\django\assets\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
